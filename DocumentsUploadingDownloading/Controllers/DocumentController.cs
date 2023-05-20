beaa30e38f66aef6e92f32f33044f7c03184f221
Dzmitry Salodki <dsalodki@gmail.com>
2023-05-20 16:18:26 +0300

    Add project files.

---
+++ DocumentsUploadingDownloading/Controllers/DocumentController.cs
@@ -0,0 +1,72 @@
+﻿using DocumentsUploadingDownloading.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DocumentsUploadingDownloading.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DocumentController : ControllerBase
+    {
+        private readonly DocumentsApiContext _db;
+        private const string _mimeType = "text/plain";
+        private const long _maxFileSize = 1048576;
+        private static readonly string[] _acceptableFileTypes = { ".txt" };
+
+        public DocumentController(DocumentsApiContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> DownloadFile(int id)
+        {
+            var doc = _db.Documents.FirstOrDefault(x => x.Id == id);
+
+            if (doc == null)
+            {
+                return NotFound();
+            }
+
+
+            return new FileContentResult(doc.Content, _mimeType)
+            {
+                FileDownloadName = doc.FileName
+            };
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadFile(IFormFile file)
+        {
+            long size = file.Length;
+            if (size > _maxFileSize || size == 0)
+            {
+                return BadRequest("File size exide than 1Mb or empty");
+            }
+
+            string fileType = Path.GetExtension(file.FileName);
+            if (!_acceptableFileTypes.Contains(fileType))
+            {
+                return BadRequest("File extension should be '.txt'");
+            }
+
+
+            using (var memoryStream = new MemoryStream())
+            {
+                await file.CopyToAsync(memoryStream);
+
+                var doc = new Document()
+                {
+                    FileName = file.FileName,
+                    Content = memoryStream.ToArray(),
+                    Create = DateTime.Now,
+                };
+
+                await _db.Documents.AddAsync(doc);
+                await _db.SaveChangesAsync();
+
+                return Ok(doc.Id);
+            }
+        }
+    }
+}
---
+++ DocumentsUploadingDownloading/Program.cs
@@ -0,0 +1,23 @@
+using DocumentsUploadingDownloading.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddEntityFrameworkNpgsql().AddDbContext<DocumentsApiContext>(
+    opt=> opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
