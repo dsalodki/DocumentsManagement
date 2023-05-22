2ad234f2b37c53edf4b2d5577c5a065f941db38b
Dzmitry Salodki <dsalodki@gmail.com>
2023-05-22 11:36:39 +0300

    remove all

--- Emailing/Models/MqDocument.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DocumentsUploadingDownloadingApi.Models
-{
-    public class MqDocument
-    {
-        public int Id { get; set; }
-        public byte[] Content { get; set; }
-        public string FileName { get; set; }
-    }
-}
